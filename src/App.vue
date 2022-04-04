c9068a62d9ac74a65ac11f140465d5d26b3d69e3
cclarice <179danil@gmail.com>
2022-04-04 13:55:50 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <header>
+    <img class="logo" src="./assets/logo.svg" alt="Optimum" height="48">
+    <section class="user card">
+
+    </section>
+  </header>
+  <nav>
+
+  </nav>
+  <main class="main">
+    <router-view />
+  </main>
+  <div id="stars" />
+  <div id="stars2" />
+  <div id="stars3" />
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss">
+@import 'styles/main';
+header {
+  margin: 16px 16px 8px;
+  width: calc(100% - 32px);
+  height: 48px;
+  display: flex;
+  justify-content: space-between;
+
+  .logo {
+
+  }
+}
+
+main {
+  min-width: min(1024px, calc(100% - 32px));
+  min-height: 256px;
+  max-height: 100%;
+  padding: 16px 0;
+  overflow-x: auto;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+}
+
+.view {
+  height: 2000px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="card view">
+    <h1> Home </h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
